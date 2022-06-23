9798d6e98ee905854913d3f1502a3a7d1a810e4d
Ayush <ayush.gangwar.cse21@itbhu.ac.in>
2022-06-23 09:54:24 +0530

    23-06-22

--- LAB/3/BST.c
+++ LAB/3/BST.c
@@ -6,6 +6,13 @@ typedef struct BST{
     struct BST *left, *right;
 } BST;
 
+BST* create_node(char val);
+void insert(BST **node, char val);
+void print_inorder(BST *node);
+void print_preorder(BST *node);
+void print_postorder(BST *node);
+int check_mirror(BST *root1, BST *root2);
+
 BST* create_node(char val){
     BST *node = (BST *)malloc(sizeof(BST));
     node->val=val;
@@ -13,54 +20,93 @@ BST* create_node(char val){
     return node;
 }
 
-void insert(BST **root, char val){
-    if(*root == NULL){
-        *root = create_node(val);
+void insert(BST **node, char val){
+    if(*node == NULL){
+        *node = create_node(val);
         return;
     }
-    if((*root)->val >= val){
-        insert(&((*root)->left), val);
+    if((*node)->val >= val){
+        insert(&((*node)->left), val);
     }
     else{
-        insert(&((*root)->right), val);
+        insert(&((*node)->right), val);
     }
 }
 
-void print_inorder(BST *root){
-    if(root == NULL){
+void print_inorder(BST *node){
+    if(node == NULL){
         return;
     }
-    print_inorder(root->left);
-    printf("%c ", root->val);
-    print_inorder(root->right);
+    print_inorder(node->left);
+    printf("%c ", node->val);
+    print_inorder(node->right);
 }
 
-void print_preorder(BST *root){
-    if(root == NULL){
+void print_preorder(BST *node){
+    if(node == NULL){
         return;
     }
-    printf("%c ", root->val);
-    print_inorder(root->left);
-    print_inorder(root->right);
+    printf("%c ", node->val);
+    print_inorder(node->left);
+    print_inorder(node->right);
 }
 
-void print_postorder(BST *root){
-    if(root == NULL){
+void print_postorder(BST *node){
+    if(node == NULL){
         return;
     }
-    print_inorder(root->left);
-    print_inorder(root->right);
-    printf("%c ", root->val);
+    print_inorder(node->left);
+    print_inorder(node->right);
+    printf("%c ", node->val);
+}
+
+int check_mirror(BST *root1, BST *root2){
+    if(root1 == NULL && root2 == NULL){
+        return 1;
+    }
+    else if(root1 == NULL || root2 == NULL){
+        return 0;
+    }
+    
+    if(root1->val != root2->val){
+        return 0;
+    }
+    return check_mirror(root1->left, root2->right) && check_mirror(root1->right, root2->left);
 }
 
 int main(){
+    // a
     BST *root = NULL;
     
-    insert(&root, 'b');
-    insert(&root, 'a');
-    insert(&root, 'c');
     insert(&root, 'd');
+    insert(&root, 'b');
+    insert(&root, 'c');
+    insert(&root, 'a');
     
+    // b
     print_inorder(root);
+    printf("\n\n");
+    
+    // c
+    BST *root1 = NULL;
+    BST *root2 = NULL;
+    
+    root1=create_node('d');
+    root1->left=create_node('b');
+    root1->right=create_node('c');
+    root1->left->left=create_node('a');
+    
+    root2=create_node('d');
+    root2->right=create_node('b');
+    root2->left=create_node('c');
+    root2->right->right=create_node('a');
+    
+    if(check_mirror(root1, root2)){
+        printf("Trees are mirror of each other!\n");
+    }
+    else{
+        printf("Trees are not mirror of each other!\n");
+    }
+    
     return 0;
 }
